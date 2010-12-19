6f418d40b858cbd1f5a58dd23de4ef0347783cff
Joshua Davey <josh@joshuadavey.com>
2010-12-18 22:01:10 -0600

    rm -rf emacs.d

--- emacs.d/bindings.el
+++
@@ -1,8 +0,0 @@
-;; Easier Window switching - Command + arrow key
-(global-set-key (kbd "s-<down>") 'windmove-down)
-(global-set-key (kbd "s-<up>") 'windmove-up)
-(global-set-key (kbd "s-<right>") 'windmove-right)
-(global-set-key (kbd "s-<left>") 'windmove-left)
-
-;; Better mark setting shortcut
-(global-set-key (kbd "M-SPC") 'set-mark-command) ;; was just-one-space
