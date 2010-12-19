6bbbcff869d890c4dbefb17ad57d05c766c5cfea
Joshua Davey <josh@joshuadavey.com>
2010-12-18 18:12:50 -0600

    Removing more unused functions

--- bash.d/init.sh
+++ bash.d/init.sh
@@ -1,7 +1,5 @@
 system_name=`uname -s` # Sets 'Darwin' or 'Linux' etc
 
-alias sprof='source ~/.bash_profile'
-
 source ~/.exports
 source ~/.aliases
 
@@ -15,16 +13,9 @@ bind "set completion-ignore-case on"
 bind "set show-all-if-ambiguous On" # this allows you to automatically show completion without double tab-ing
 
 shopt -s histappend
-# shopt -s checkwinsize
-# shopt -s globstar
 
 # Auto-completions
 complete -C ~/.bash.d/rake-completion.rb -o default rake
-
-# Work computer overrides
-if [[ $HOME == "/Users/josh" ]]; then
-	export GEMDIR="$HOME/.gem/ruby/1.8"
-fi
 
 # changing directory to code project
 function c { cd ~/current/$1; }
